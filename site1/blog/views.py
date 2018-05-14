db8a032d81160569f0195cf4d278335cffe2efc6
Jinup Kim <kimjinup@gmail.com>
2018-05-14 09:09:23 +0900

    second commit

--- site1/blog/views.py
+++ site1/blog/views.py
@@ -9,6 +9,10 @@ from blog.models import Post
 
 def post_list(request):
 	posts = Post.published.all()
-	return render(request
+	return render(request, 'blog/post/list.html', {'posts': posts})
+
+def post_detail(request, year, month, day, post):
+	posts = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
+	return render(request, 'blog/post/detail.html', {'post': post})
 
 
